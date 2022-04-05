bedad7f11b2e1e216d972d98e45f3d1ef7d38d88
ryanchan8662 <96643993+ryanchan8662@users.noreply.github.com>
2022-04-05 15:29:24 -0400

    Error changes to subsystemUI
    Fixed hashmap and arraylist errors with loops

--- Sample-Code/cryptoTrader/src/main/java/cryptoTrader/main/SubsystemUI.java
+++ Sample-Code/cryptoTrader/src/main/java/cryptoTrader/main/SubsystemUI.java
@@ -29,26 +29,33 @@ public class SubsystemUI {
     public ResultFactory runTrades () {
         ResultFactory results = new ResultFactory();
         // get prices of all subscribed coins
-        for (TradingClient current : this.activeClients.toArray()) {
-            this.coins.addAll(current.getCoins()); // add coins to accumulator list, removing duplicates
+        for (int i = 0; i < this.activeClients.size(); i++) { // for all clients
+        	// add coins to accumulator list, removing duplicates
+        	String[] coins = this.activeClients.get(i).getCoins();
+        	for (int j = 0; j < coins.length; j++) {
+        		this.coins.add(coins[j]);
+        	}
         }
         // call API and return coins to hashmap
 
         HashMap<String, Float> coinPrices = new HashMap<String, Float>(); // API returns here <-- (temp empty)
 
-        for (TradingClient current : this.activeClients.toArray()) { // for every client
-            ArrayList<String> clientCoinList = new ArrayList<String>();
+        for (int i = 0; i < this.activeClients.size(); i++) {
+        	ArrayList<String> clientCoinList = new ArrayList<String>();
             HashMap<String, Float> clientCoinPrices = new HashMap<String, Float>();
-            for (String clientCoins : current.getCoins()) { // for every coin of a specific client
-                clientCoinList.add(coinPrices.get(clientCoins));
-                clientCoinPrices.put(clientCoins, coinPrices.get(clientCoins));
+            TradingClient currentClient = this.activeClients.get(i);
+            for (String clientCoins : this.activeClients.get(i).getCoins()) { // for every coin of a specific client
+            	String[] coins = this.activeClients.get(i).getCoins();
+            	for (int j = 0; j < coins.length; j++) {
+            		clientCoinList.add(coins[i]);
+            		clientCoinPrices.put(clientCoins, coinPrices.get(clientCoins));
+            	}
+            	// pass coin prices over to trading client
+                TradeResult clientResult = this.activeClients.get(i).runStrategy(clientCoinPrices);
+                results.createResult(this.activeClients.get(i).name(), this.activeClients.get(i).strategy(), "Undefined coin", clientResult, 6.9f, 100000l);
+                // placeholders used for some values, will be replaced later
             }
-            // pass coin prices over to trading client
-            TradeResult clientResult = current.runStrategy(clientCoinPrices);
-            results.createResult(current.name(), current.strategy(), "Undefined coin", clientResult, 6.9f, 100000l);
-            // placeholders used for some values, will be replaced later
-        }
-        return (results); // returns class holding all results
-        
+            
+        } return (results); // returns class holding all results
     }
 }
